1ff6072f58d7ea6b325b3e7360ad6ca84f408228
MalazeKL <88525394+MalazeKL@users.noreply.github.com>
2022-10-21 19:43:02 +0700

    Update dien_tich_tam_giac.py

--- dien_tich_tam_giac.py
+++ dien_tich_tam_giac.py
@@ -4,4 +4,4 @@ b = float(input("chieu dai canh b: "))
 c = float(input("chieu dai canh c: "))
 P = (a + b + c) / 2
 dien_tich = math.sqrt( P * ( P-a) * (P-b) * (P-c))
-print(" Area of the triangle is: ", dien_tich)
+print(" dien tich tam giac la: ", dien_tich)
